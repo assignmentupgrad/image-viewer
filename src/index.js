8cb7b670ceb775680ddea1ea3030a14f2ed79915
SonaliBhagwatkar <sonali.bhagwatkar@gmail.com>
2019-08-22 23:41:13 +0530

    Developed Header on the Login Page

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+import Login from './screens/login/Login';
 
-ReactDOM.render(<div>Image Viewer</div>, document.getElementById('root'));
+ReactDOM.render(<Login />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
