559a9373c655bbbb70ef1ae4764192dab3684445
SonaliBhagwatkar <sonali.bhagwatkar@gmail.com>
2019-08-27 12:40:21 +0530

    Implemented Search Box - code completed

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 import './Header.css';
+import logo from '../../assets/logo.svg';
+import Input from '@material-ui/core/Input';
+import FormControl from '@material-ui/core/FormControl';
 
 class Header extends Component {
        render(){
@@ -7,7 +10,13 @@ class Header extends Component {
             <div>
                <header className="app-header">
                     <p className="logo">Image Viewer</p>
-
+                    {/* Search Box Added */}
+                    <div className="searchBox">
+                    <img src={logo} className="app-logo" alt="Search Logo"/> 
+                    <FormControl className="formControl">
+                      <Input className="searchText" type="text" placeholder="Search..." disableUnderline="true"/>
+                    </FormControl>
+                    </div>
                 </header><br/><br/>
             </div>
         )
