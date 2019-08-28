01addea17d60d0ad37759a4e071d3ece44b653d9
SonaliBhagwatkar <sonali.bhagwatkar@gmail.com>
2019-08-28 11:11:32 +0530

    Completed code for develiping user profile icon

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -3,21 +3,37 @@ import './Header.css';
 import logo from '../../assets/logo.svg';
 import Input from '@material-ui/core/Input';
 import FormControl from '@material-ui/core/FormControl';
+import IconButton from '@material-ui/core/IconButton';
+
 
 class Header extends Component {
-       render(){
-        return(
+
+        
+  
+  render(){
+            return(
             <div>
                <header className="app-header">
-                    <p className="logo">Image Viewer</p>
-                    {/* Search Box Added */}
-                    <div className="searchBox">
+                   <p className="logo">Image Viewer</p>
+                      {/* Search Box Added  */}
+                    {this.props.showSearchBox === "true" ?
+                    <div className="searchBox" >
                     <img src={logo} className="app-logo" alt="Search Logo"/> 
                     <FormControl className="formControl">
-                      <Input className="searchText" type="text" placeholder="Search..." disableUnderline="true"/>
+                      <Input className="searchText" type="text" placeholder="Search..." disableUnderline={true}/>
                     </FormControl>
-                    </div>
-                </header><br/><br/>
+                     </div>: ""}
+                     {/* User Profile Icon Added  */}
+                     {this.props.loggedIn === true ?
+                      <IconButton 
+                      color="primary" 
+                      className="iconBtn" 
+                      aria-label="add a profile pic"
+                      edge="start"
+                      size="medium">
+                        <img className="profilePic" src = {this.props.profilePic} alt="logged in user profile pic"/>
+                        </IconButton> : ""}
+                     </header><br/><br/>
             </div>
         )
     }
--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -2,16 +2,28 @@ import React, { Component } from 'react';
 import './Home.css';
 import Header from '../../common/header/Header';
 
+
 class Home extends Component{
 
-        componentDidMount() {
+        constructor(){
+            super();
+            this.state = {
+                profilePic: [{}],
+                loggedIn: sessionStorage.getItem("access_token") === "null" ? false : true,
+            }
+        }
 
-            //call to API Endpoint 1
+        UNSAFE_componentWillMount() {
+
+            //call to API Endpoint 1 to get profile-picture
             
             let xhrEndPt1 = new XMLHttpRequest();
+            let that = this;
             xhrEndPt1.addEventListener("readystatechange", function(){
                 if (this.readyState === 4) {
-                   console.log(JSON.parse(this.responseText));
+                    console.log(JSON.parse(this.responseText).data.profile_picture);
+                    that.setState({profilePic: JSON.parse(this.responseText).data.profile_picture});
+                    console.log(that.state.profilePic);
                 }
             });
             xhrEndPt1.open("GET", this.props.baseUrl + "?access_token=8661035776.d0fcd39.39f63ab2f88d4f9c92b0862729ee2784");
@@ -23,19 +35,26 @@ class Home extends Component{
             xhrEndPt2.addEventListener("readystatechange", function(){
                 if (this.readyState === 4) {
                    console.log(JSON.parse(this.responseText));
+
                 }
             });
             xhrEndPt2.open("GET", this.props.baseUrl + "media/recent?access_token=8661035776.d0fcd39.39f63ab2f88d4f9c92b0862729ee2784");
             xhrEndPt2.send(null);
+
+            
+
         }
          
      render() {
                return(
             <div>
-                <Header baseUrl={this.props.baseUrl}/>, 
+                <Header 
+                baseUrl={this.props.baseUrl}
+                showSearchBox="true" 
+                profilePic={this.state.profilePic} 
+                loggedIn={this.state.loggedIn}/> 
             </div>
-               )
-    }
+               )}
 }
 
 export default Home;
